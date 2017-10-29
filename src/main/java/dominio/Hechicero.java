9c77bec2e8d3f2503e7b414e7cea4d22e77bf951
Unknown <castagna.cristian16@gmail.com>
2017-10-29 15:45:42 -0300

    Se solucionan Incidencias en Hechicero.java

--- src/main/java/dominio/Hechicero.java
+++ src/main/java/dominio/Hechicero.java
@@ -2,7 +2,9 @@
 package dominio;
 
 /**
- * La clase Hechicero es una casta de Personaje, hereda de la clase Casta. Posee dos constructores El otro constructor, llama al constructor de la clase padre (Casta), pasándole los argumentos recibidos por el constructor hijo.
+ * La clase Hechicero es una casta de Personaje, hereda de la clase Casta.
+ * Posee dos constructores El otro constructor, llama al constructor de la
+ * clase padre (Casta), pasándole los argumentos recibidos por el constructor hijo.
  */
 public class Hechicero extends Casta {
 	/**
@@ -23,8 +25,9 @@ public class Hechicero extends Casta {
 	private static final int BONUSINTELIGENCIA = 5;
 
 	/**
-	 * La clase Hechicero es una casta de Personaje, hereda de la clase Casta. Posee dos constructores El otro constructor, llama al constructor de la clase padre (Casta), pasándole los argumentos recibidos por el constructor hijo.
-	 * 
+	 * La clase Hechicero es una casta de Personaje, hereda de la clase Casta.
+	 * Posee dos constructores El otro constructor, llama al constructor de la
+	 * clase padre (Casta), pasándole los argumentos recibidos por el constructor hijo.
 	 * @param probCrit
 	 *            Probabilidad de que el personaje realice un golpe crítico
 	 * @param evasion
@@ -39,19 +42,26 @@ public class Hechicero extends Casta {
 	}
 
 	/**
-	 * El constructor por defecto, llama al constructor por defecto de la clase padre (Casta), inicializando probabilidadGolpeCritico, probabilidadEvitarDaño, dañoCritico, con valores predeterminados.
+	 * El constructor por defecto, llama al constructor por defecto de la clase
+	 * padre (Casta), inicializando probabilidadGolpeCritico, probabilidadEvitarDaño,
+	 * dañoCritico, con valores predeterminados.
 	 */
 	public Hechicero() {
 		super();
 	}
 
 	/**
-	 * Retorna un booleano dependiendo de si se realizó exitosamente o no el ataque. La primera condición para que el ataque pueda realizarse es que el atacante(caster) posea 10 o más del atributo energia ya que estos se descuentan seguido de comprobar que los posee, de lo contrario el ataque no será posible y se retornará false. El parámetro caster a su vez llama al método calcularPuntosDeMagia() el cual luego se multiplica por 1.5
-	 * 
+	 * Retorna un booleano dependiendo de si se realizó exitosamente o no el ataque.
+	 * La primera condición para que el ataque pueda realizarse es que el
+	 * atacante(caster) posea 10 o más del atributo energia ya que estos se descuentan
+	 * seguido de comprobar que los posee, de lo contrario el ataque no será posible y
+	 * se retornará false. El parámetro caster a su vez llama al método
+	 * calcularPuntosDeMagia() el cual luego se multiplica por 1.5
 	 * @param caster
 	 *            Personaje que realiza la habilidad
 	 * @param atacado
-	 *            puede ser una instancia de Persona o NPC dependiendo de la misma, variará lo que retornará serAtacado()
+	 *            puede ser una instancia de Persona o NPC dependiendo de la misma,
+	 *            variará lo que retornará serAtacado()
 	 * @return booleano que determina si el ataque fue exitoso o no.
 	 */
 	@Override
@@ -61,16 +71,22 @@ public class Hechicero extends Casta {
 			caster.reducirEnergia(ENERGIAMINIMA);
 			tieneEnergia = true;
 		}
-		return tieneEnergia && (atacado.serAtacado((int) (caster.calcularPuntosDeMagia() * MULTIPLICADORMAGIA)) > 0);
+		return tieneEnergia && (atacado.serAtacado((int) (caster.calcularPuntosDeMagia()
+				* MULTIPLICADORMAGIA)) > 0);
 	}
 
 	/**
-	 * Retorna un booleano dependiendo de si se realizó exitosamente o no el ataque. La primera condición para que el ataque pueda realizarse es que el atacante(caster) posea 10 o más del atributo energia ya que estos se descuentan seguido de comprobar que los posee, de lo contrario el ataque no será posible y se retornará false. Para que éste método tenga posibilidad de retornar true, aliado debe ser de la instancia Personaje
-	 * 
+	 * Retorna un booleano dependiendo de si se realizó exitosamente o no el ataque.
+	 * La primera condición para que el ataque pueda realizarse es que el
+	 * atacante(caster) posea 10 o más del atributo energia ya que estos se descuentan
+	 * seguido de comprobar que los posee, de lo contrario el ataque no será posible y
+	 * se retornará false. Para que éste método tenga posibilidad de retornar true,
+	 * aliado debe ser de la instancia Personaje
 	 * @param caster
 	 *            Personaje que realiza la habilidad
 	 * @param aliado
-	 *            atacado puede ser una instancia de Persona o NPC dependiendo de la misma podrá o no retornar true el método.
+	 *            atacado puede ser una instancia de Persona o NPC dependiendo de la
+	 *            misma podrá o no retornar true el método.
 	 * @return booleano que determina si el ataque fue exitoso o no.
 	 */
 	@Override
@@ -86,12 +102,17 @@ public class Hechicero extends Casta {
 	}
 
 	/**
-	 * Retorna un booleano dependiendo de si se realizó exitosamente o no el ataque. La primera condición para que el ataque pueda realizarse es que el atacante(caster) posea 10 o más del atributo energia ya que estos se descuentan seguido de comprobar que los posee, de lo contrario el ataque no será posible y se retornará false. Para que éste método tenga posibilidad de retornar true, el Atacado debe ser una instancia Personaje
-	 * 
+	 * Retorna un booleano dependiendo de si se realizó exitosamente o no el ataque.
+	 * La primera condición para que el ataque pueda realizarse es que el
+	 * atacante(caster) posea 10 o más del atributo energia ya que estos se descuentan
+	 * seguido de comprobar que los posee, de lo contrario el ataque no será posible y
+	 * se retornará false. Para que éste método tenga posibilidad de retornar true, el
+	 * Atacado debe ser una instancia Personaje
 	 * @param caster
 	 *            Personaje que realiza la habilidad
 	 * @param atacado
-	 *            El atacado puede ser una instancia de Persona o NPC dependiendo de la misma podrá o no retornar true el método.
+	 *            El atacado puede ser una instancia de Persona o NPC dependiendo de la
+	 *            misma podrá o no retornar true el método.
 	 * @return retorna si se ejecutó correctamente la habilidad.
 	 */
 	@Override
@@ -99,8 +120,10 @@ public class Hechicero extends Casta {
 		if (caster.getEnergia() >= ENERGIAMINIMA) {
 			caster.reducirEnergia(ENERGIAMINIMA);
 			if (atacado instanceof Personaje) {
-				int energiaRobada = ((Personaje) atacado).serDesernegizado(caster.calcularPuntosDeMagia());
-				int saludRobada = ((Personaje) atacado).serRobadoSalud(caster.calcularPuntosDeMagia() / DIVISORDEMAGIA);
+				int energiaRobada = ((Personaje) atacado).serDesernegizado(
+						caster.calcularPuntosDeMagia());
+				int saludRobada = ((Personaje) atacado).serRobadoSalud(
+						caster.calcularPuntosDeMagia() / DIVISORDEMAGIA);
 				caster.serEnergizado(energiaRobada);
 				caster.serCurado(saludRobada);
 				return true;
@@ -112,7 +135,6 @@ public class Hechicero extends Casta {
 
 	/**
 	 * Retorna un entero que otorga el bonus de inteligencia perteneciente a esta casta.
-	 * 
 	 * @return Retorna la inteligencia extra de la casta.
 	 */
 	@Override
@@ -122,7 +144,6 @@ public class Hechicero extends Casta {
 
 	/**
 	 * Retorna un entero que otorga el bonus de destreza perteneciente a esta casta.
-	 * 
 	 * @return Retorna 0 para esta casta.
 	 */
 	@Override
@@ -132,7 +153,6 @@ public class Hechicero extends Casta {
 
 	/**
 	 * Retorna un entero que otorga el bonus de fuerza perteneciente a esta casta.
-	 * 
 	 * @return Retorna 0 para esta casta.
 	 */
 	@Override
@@ -142,7 +162,6 @@ public class Hechicero extends Casta {
 
 	/**
 	 * Retorna una string con el nombre de la casta.
-	 * 
 	 * @return Retorna el nombre de la casta.
 	 */
 	@Override
@@ -152,12 +171,11 @@ public class Hechicero extends Casta {
 
 	/**
 	 * Retorna un vector de string con los nombres de las habilidades de la casta.
-	 * 
 	 * @return Retorna nombres de las habilidades propias de la casta.
 	 */
 	@Override
 	public final String[] getHabilidadesCasta() {
-		return new String[] { "Bola de Fuego", "Curar Aliado", "Robar Energia y Salud" };
+		return new String[] {"Bola de Fuego", "Curar Aliado", "Robar Energia y Salud"};
 	}
 
 }
